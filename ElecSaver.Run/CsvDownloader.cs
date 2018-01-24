b1777b6a8e9ba88ad1d8b6de51148c3d6b21f75e
DESKTOP-5ND3FOT\Mohamed <mohamed.abozyd@gmail.com>
2018-01-24 20:47:53 +1300

    Add Solution for  the projects and add downloader

---
+++ ElecSaver.Run/CsvDownloader.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ElecSaver.Run
+{
+    public class CsvDownloader
+    {
+        public string Url { get; set; }
+        public async Task<string> DownloadAsync()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(Url);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ ElecSaver.Run/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ElecSaver.Run
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            CsvDownloader downloader = new CsvDownloader();
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string url = $"https://www1.electricityinfo.co.nz/download/prices?search_form[run_types][]=A&search_form[run_types][]=I&search_form[market_types][]=E&search_form[nodes][]=BEN2201&search_form[nodes][]=HAY2201&search_form[nodes][]=OTA2201&search_form[date_from]={date}&search_form[date_to]={date}&search_form[tp_from]=1&search_form[tp_to]=48&search_form[tp_roll_back]=7&search_form[tp_roll_fwd]=7";
+            downloader.Url = url;
+            string content = await downloader.DownloadAsync();
+            Console.WriteLine(content);
+            Console.ReadLine();
+        }
+    }
+}
